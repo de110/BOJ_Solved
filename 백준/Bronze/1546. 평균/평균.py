2b5560a871871007195ef072b89e6b16820b3821
de110 <67581448+de110@users.noreply.github.com>
2023-06-16 10:00:26 +0900

    [Bronze I] Title: 평균, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,7 +1,4 @@
 import sys
-B=[]
 N = int(sys.stdin.readline())
 A = list(map(int,sys.stdin.readline().split()))
-for i in range(N):
-    B.append(A[i]/max(A)*100)
-print(sum(B)/N)
+print(sum(A)*100/max(A)/N)
