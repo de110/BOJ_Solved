1cb93e808ecf45270d0743f3b8e8036cfb1bcbd5
이다은 <67581448+de110@users.noreply.github.com>
2024-02-23 00:35:21 +0900

    [Silver V] Title: 셀프 넘버, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int d(int num){
+    int sum = num;
+    while(num!=0){
+        sum = sum + (num%10);
+        num = num/10;
+    }
+    return sum;
+}
+
+int main(){
+    bool check[10001]{false};
+    for(int i=1;i<10001;i++){
+        int n = d(i);
+        if(n < 10001){
+            check[n]=true;
+        }
+    }
+    for(int i=1;i<10001;i++){
+        if(!check[i]){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
