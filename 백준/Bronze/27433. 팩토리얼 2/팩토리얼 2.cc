174402c8635d99ee6bb1613eeecacd753a15a6d2
이다은 <67581448+de110@users.noreply.github.com>
2025-03-23 15:32:33 +0900

    Update 팩토리얼 2.cc

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 
+long long factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+}
+
 int main() {
     int N;
     cin >> N;
-
-    long long result = 1;
-    for (int i = 2; i <= N; ++i) {
-        result *= i;
-    }
-
-    cout << result << '\n';
+    cout << factorial(N) << '\n';
     return 0;
 }
